c2013dc47077f567aacde29495a4a82523f8b49d
Đạt <lety_20391@yahoo.com>
2017-11-18 17:42:19 +0700

    use Angular to display Detail Product
    via localStorage("proID", " ");

---
+++ Data/detailData.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+var productDetail = [
+	{
+		ID : "001",
+		Name : "Buckwheat",
+		Detail : "High in protein (13-15%), second highest only to oats, and rich in the amino acid lysine. Rich in carbohydrates (mainly starch). Rich in polyunsaturated essential fatty acids, such as linoleic acid.Contains vitamins B1, C and E. Contains higher levels of zinc, copper, and manganese than other cereal grains, and the bioavailability of these minerals is also quite high. High in soluble fibre.",
+		Price : "600",
+		Off : "0.5",
+		Img : "image/Cereal/001_buckwheat_big.jpg"
+	},
+	{
+		ID : "002",
+		Name : "Chia",
+		Detail : "Chia is grown commercially for its seed, a food rich in omega-3 fatty acids since the seeds yield 25–30% extractable oil, including α-linolenic acid. The composition of the fat of the oil may be 55% ω-3, 18% ω-6, 6% ω-9, and 10% saturated fat.",
+		Price : "700",
+		Off : "0.3",
+		Img : "image/Cereal/002_chia_big.jpg"
+	}
+];
