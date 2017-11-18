705eb354279172f2c162001e155773225f31e79c
Đạt <lety_20391@yahoo.com>
2017-11-18 16:18:41 +0700

    use AngularJs to display Query page

---
+++ Query Data/querydata.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+var queryData=[
+	{
+		id : "01",
+		question : "1. Is organic food certified?",
+		answer : "All organic food sold by our farm has to be certified by registered certification bodies.Certification offers you an independent, third-party assurance that the products you purchase have been produced to full organic standards, all the way from the field to your shopping basket. Organic farmers and food processors abide by rules, or standards.They are independently inspected and certified, which involves an inspector actually visiting their farms and factories to see what is going on. The standards for organic food are laid down in European law and in some areas in the world."
+	},
+	{
+		id : "02",
+		question : "2. What different between your organic food and other organic food?",
+		answer : "All our organic food comes from trusted sources and meets strict standards laid down in European law and in some areas in the world which give consumers confidence in the food they buy. Organic food with the Association’s symbol has been certified to higher standards where our expert standards committees have concluded this is necessary. For example, no system of farming requires higher levels of animal welfare than organic farms working to Association standards. We’ve led the way in setting organic standards and have gone further than any other  organic standards setter to make sure the standards we publish are clear. Nevertheless, they have evolved over decades and now run to hundreds of pages. So they could be even clearer, simpler and have greater impact."
+	},
+	{
+		id : "03",
+		question : "3. Why should we buy organic food here?",
+		answer : "Benefits of Organic Farming: Healthy Foods, Low Incidence of Pests. We champion a world where people, farm animals and nature can thrive. If you want to eat clean food, safe, reliable source and good services. You can pick, eat and shop for good. If everyone who visits our website, looks out for our logo on organic food, and cares about the food they eat and how it’s been produced. It allows us to contribute to the health of yours and your family."
+	},
+	{
+		id : "04",
+		question : "4. What are your outstanding products?",
+		answer : "Our outstanding product are soybean, corn, rice, nutritious cereals and cooking oil."
+	}
+];
